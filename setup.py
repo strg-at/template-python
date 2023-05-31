cc87847758a57a908d00f09be71799821ddc40ee
codechannel <dadhichi.shukla@strg.at>
2023-05-31 10:40:18 +0200

    feat: configure setup file for packages. INPRO-1564

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+setup
+"""
+from setuptools import setup
+
+setup(
+    name='template_python',
+    description='Template Python used in strg.AGENTS',
+    author='strg.at',
+    packages=[
+        'template_python.template_python_package'
+    ],
+    namespace_packages=['template_python'],
+    author_email='support@strg.at',
+    license='LGPL',
+    version="1.0"
+)
